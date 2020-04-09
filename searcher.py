7963e101751127cd8ddf110813718572aa113079
Sagar Bade Shrestha <itssagarshrestha@gmail.com>
2020-04-09 17:38:33 +0545

    now email as well

--- searcher.py
+++ searcher.py
@@ -17,8 +17,26 @@ def phone(text) :
     return ph_nums
 
 
-#email
+def mail(text) :
+    mail_re = re.compile(r'''
+            [0-9a-zA-Z-_%+.]+       #name
+            @               
+            [0-9a-zA-Z-]+           #domain name
+            \.[0-9a-zA-Z-]+         #.com or sth
+            (\.[a-zA-Z]{2,4})?      #.np or sth
+            ''',re.VERBOSE)
+    
+    #iterating through each match and appending it to ph_list
+    mail_list = []
+    for ans in re.finditer(mail_re, text) :
+        mail_list.append(str(ans.group(0)))
+
+    #converting list into string
+    emails = '\n'.join(mail_list)
+    return emails
+
 
 text = pyperclip.paste()
 #phone(text)
+print(mail(text))
 print(phone(text))
