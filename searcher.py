facecc0d49d598e2795e5cf3f943d3b0fe247e1a
Sagar Bade Shrestha <itssagarshrestha@gmail.com>
2020-04-09 17:02:47 +0545

    can do landlines too now

--- searcher.py
+++ searcher.py
@@ -3,8 +3,9 @@ import pyperclip, re
 def phone(text) :
     phone_re = re.compile(r'''
             ((\(\+\d{3}\)-)|(\+\d{3}-)|(\+\d{3}\s))?         # (+977) or +977 or none
-            ((\d{4}-\d{3}-\d{3})|(\d{10}))    #10-digit phone number
-    ''',re.VERBOSE)
+            (((\d{4}-\d{3}-\d{3})|(\d{10}))|(\d{1,3}-\d{5,6}))    #10-digit phone number or 6-7 digit landline number
+
+            ''',re.VERBOSE)
     
     #iterating through each match and appending it to ph_list
     ph_list = []
@@ -15,7 +16,6 @@ def phone(text) :
     ph_nums = '\n'.join(ph_list)
     return ph_nums
 
-#landline
 
 #email
 
